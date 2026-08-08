8b9c0422ebff6bb55cd420fb50dac6f983baf5c7
Natalia <kara333@mail.ru>
2026-08-08 14:07:42 +0300

    update maps api key

--- src/services/mapService.js
+++ src/services/mapService.js
@@ -1,3 +1,5 @@
+const API_KEY = import.meta.env.VITE_YANDEX_MAPS_KEY;
+
 export function getStaticMapUrl(latitude, longitude, width, height, zoom = 12) {
     const params = new URLSearchParams({
         ll: `${longitude},${latitude}`,
@@ -5,6 +7,7 @@ export function getStaticMapUrl(latitude, longitude, width, height, zoom = 12) {
         size: `${width},${height}`,
         l: 'map',
         pt: `${longitude},${latitude},pm2dgm`,
+        apikey: API_KEY,
     });
 
     return `https://static-maps.yandex.ru/1.x/?${params}`;
